12e6179f5196d78b35b6cf7d4de58474877a99d0
aliabdulbaqi01 <aliabdulbaqi01@gmail.com>
2024-09-19 18:33:22 +0300

    create a model class

---
+++ model.php
@@ -0,0 +1,15 @@
+<?php
+
+class Model
+{
+    public function getData(): array
+    {
+        $dsn = 'mysql:host=localhost;dbname=auth_php';
+        $pdo = new PDO($dsn, 'root', '', [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]);
+        $stmt = $pdo->query("SELECT * FROM product");
+
+        return $stmt->fetchAll();
+    }
+}
